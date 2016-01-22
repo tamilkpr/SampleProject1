45a26b121c143b5241b26bd44398b06b14834cd6
Tamilmani A <tamilmani@vegamsoft.com>
2016-01-22 10:50:43 +0530

    First version

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(new DateTime());
+            //Console.WriteLine((new DateTime(9999, 1, 1) - new DateTime(1, 1, 1)).Days);
+            //TimeSpan t = new TimeSpan(19000107, 0, 0);
+            //Trace.Listeners.Add(new TextWriterTraceListener(@"e:\test.log"));
+            //Trace.AutoFlush = true;
+            //Trace.Indent();
+            //Trace.WriteLine("Entering Main");
+            //Console.WriteLine("Hello World.");
+            //Trace.WriteLine("Exiting Main");
+            //Trace.Unindent();
+
+            Log("Tamilmani");
+        }
+
+        static void Log(object message)
+        {
+            StackFrame frame = new StackFrame(1, true);
+            var method = frame.GetMethod();
+            var fileName = frame.GetFileName();
+            var lineNumber = frame.GetFileLineNumber();
+            Console.WriteLine("{0}({1}):{2} - {3}", fileName, lineNumber, method.Name, message);
+        }
+    }
+}
---
+++ MvcApplication1/EmployeeMgmt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace MvcApplication1
+{
+    public class EmployeeDetails
+    {
+        public int EmpId;
+        public string EmployeeName;
+        public string Mobile;
+        public DateTime JoinedDate{get;set;}
+        public string Address;
+        public string Position;
+    }
+
+    public class EmployeeMgmt
+    {
+        public static List<EmployeeDetails> GetAllEmployees()
+        {
+            List<EmployeeDetails> employees = new List<EmployeeDetails>();
+            string xmlFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "employees.xml");
+            if (File.Exists(xmlFilePath))
+            {
+                XElement xElt = XElement.Load(xmlFilePath);
+            }
+
+            return employees;
+        }
+    }
+}
